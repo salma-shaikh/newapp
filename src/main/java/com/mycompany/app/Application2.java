0549fb42fbaa64a71d56ade26511f4966f1c5509
Ubuntu <ubuntu@ip-172-31-37-191.us-east-2.compute.internal>
2020-03-31 13:01:48 +0000

    adding second src file

---
+++ src/main/java/com/mycompany/app/Application2.java
@@ -0,0 +1,18 @@
+package com.mycompany.app;
+
+
+
+public class Application2{
+
+
+
+    public static void main(String[] args) {
+
+System.out.println("helloo golu....i LOVE U");
+
+
+
+    }
+
+}
+
