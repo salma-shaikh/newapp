717ec94bf31e3c4f1107861a1073a67ee193f159
Ubuntu <ubuntu@ip-172-31-37-191.us-east-2.compute.internal>
2020-03-31 13:58:15 +0000

    fourth src added

---
+++ src/main/java/com/mycompany/app/Application4.java
@@ -0,0 +1,18 @@
+package com.mycompany.app;
+
+
+
+public class Application4 {
+
+
+
+    public static void main(String[] args) {
+
+System.out.println("My fourth Application");
+
+
+
+    }
+
+}
+
